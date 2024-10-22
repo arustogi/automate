d8cd72b5747a950e5b2f0a0b23803f05143b931d
arustogi <arustogi@andrew.cmu.edu>
2024-10-22 15:36:04 -0400

    fixed upload bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2 import service_account
@@ -15,6 +15,7 @@ if missing_padding:
 key_json = base64.b64decode(encoded_key).decode('utf-8')
 credentials = service_account.Credentials.from_service_account_info(json.loads(key_json))
 drive_service = build('drive', 'v3', credentials=credentials)
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -23,18 +24,24 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['pdf']
         if file:
-            filename = file.filename
-            filepath = os.path.join('/your/upload/folder', filename)
-            file.save(filepath)
-            return redirect(url_for('home'))  
+            file.save(file.filename)
+            media = MediaFileUpload(file.filename, mimetype='application/pdf')
+            drive_service.files().create(
+                media_body=media,
+                body={'name': file.filename, 'parents': ['1kP7PS88Z7d71-9x80UmRaSaQsey1QY0P']}  # Add the correct folder ID
+            ).execute()
+            os.remove(file.filename)
+            return render_template('upload.html', message="File uploaded successfully!")
     return render_template('upload.html')
-
 # PowerPoint Presentation Route
 @app.route('/presentation')
 def presentation():
     return render_template('presentation.html')
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
