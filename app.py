90c91b691600cd04bad0edfed3cf2b780db2a4c1
arustogi <arustogi@andrew.cmu.edu>
2024-10-22 14:37:22 -0400

    Update app to use base64 encoded credentials

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask, request, render_template
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2 import service_account
+import base64
+import json
 import os
 app = Flask(__name__)
-SERVICE_ACCOUNT_FILE = 'service-account.json'
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
+encoded_key = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
-)
+# Decode the base64 string back to JSON format
+key_json = base64.b64decode(encoded_key).decode('utf-8')
+
+# Load the JSON key into credentials
+credentials = service_account.Credentials.from_service_account_info(json.loads(key_json))
 drive_service = build('drive', 'v3', credentials=credentials)
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
